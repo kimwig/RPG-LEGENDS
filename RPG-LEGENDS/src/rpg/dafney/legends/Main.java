5301ba8ce4b949772bff46584197d4824ee7ee42
DafneyAtlas <dafney@swemine.com>
2019-06-12 16:29:15 +0200

    Scheduler register etc

--- RPG-LEGENDS/src/rpg/dafney/legends/Main.java
+++ RPG-LEGENDS/src/rpg/dafney/legends/Main.java
@@ -1,20 +1,45 @@
 package rpg.dafney.legends;
 
+import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import rpg.dafney.legends.functions.Config;
 import rpg.dafney.legends.functions.Timers;
 
 public class Main extends JavaPlugin
 {
+	// ** Plugin
+	public static Plugin plugin;
+	public static Server server;
+	public static PluginManager pluginManager;
+	public static BukkitScheduler scheduler;
+	
 	// ** onEnable
 	public void onEnable()
 	{
+		// ** Plugin
+		plugin = this;
+		
+		// ** Server
+		server = plugin.getServer();
+		
+		// ** Manager
+		pluginManager = server.getPluginManager();
+		
+		// ** Scheduler
+		scheduler = server.getScheduler();
+		
 		// ** Load Config
 		Config.Load();
 		
 		// ** Load Timers
 		Timers.Start();
+		
+		// ** Register Listener
+		pluginManager.registerEvents(new MainListener(), this);
 	}
 	
 	// ** onDisable
@@ -22,5 +47,11 @@ public class Main extends JavaPlugin
 	{
 		// ** Save Config
 		Config.Save();
+		
+		// ** Reset Tasks
+		scheduler.cancelTasks(plugin);
+		
+		// ** Reset Plugin
+		plugin = null;
 	}
 }
