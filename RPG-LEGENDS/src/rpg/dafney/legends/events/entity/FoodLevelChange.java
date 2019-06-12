27e312316f68858eedda8be06d7e6e271eae69f6
DafneyAtlas <dafney@swemine.com>
2019-06-12 21:34:46 +0200

    Food handle

--- RPG-LEGENDS/src/rpg/dafney/legends/events/entity/FoodLevelChange.java
+++ RPG-LEGENDS/src/rpg/dafney/legends/events/entity/FoodLevelChange.java
@@ -1,5 +1,6 @@
 package rpg.dafney.legends.events.entity;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 public class FoodLevelChange 
@@ -7,6 +8,13 @@ public class FoodLevelChange
 	// ** Event
 	public static void onEvent(FoodLevelChangeEvent event)
 	{
+		// ** Player
+		Player player = (Player) event.getEntity();
+		
+		// ** Reset
+		player.setSaturation(200);
+		player.setExhaustion(0);
+		
 		// ** Cancel event
 		event.setCancelled(true);
 
