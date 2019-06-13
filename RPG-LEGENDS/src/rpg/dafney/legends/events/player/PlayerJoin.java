8c657d57d71803362888a3ecc5eba5d3ab4b30b8
Happyfaceing <wallsylt@gmail.com>
2019-06-14 01:01:09 +0200

    Update PlayerJoin.java

--- RPG-LEGENDS/src/rpg/dafney/legends/events/player/PlayerJoin.java
+++ RPG-LEGENDS/src/rpg/dafney/legends/events/player/PlayerJoin.java
@@ -1,8 +1,14 @@
 package rpg.dafney.legends.events.player;
 
+import java.lang.reflect.Field;
+
+import org.bukkit.craftbukkit.v1_14_R0.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import net.minecraft.server.v1_14_R0.IChatBaseComponent;
+import net.minecraft.server.v1_14_R0.PacketPlayOutPlayerListHeaderFooter;
+import net.minecraft.server.v1_14_R0.IChatBaseComponent.ChatSerializer;
 import rpg.dafney.legends.functions.Texts;
 import rpg.dafney.legends.utils.Utils;
 
@@ -16,6 +22,9 @@ public class PlayerJoin
 		
 		// ** JoinMessage
 		PlayerJoin.sendJoinMessage(player, event);
+		
+		// ** Header & Footer
+		sendHeaderFooter(player, Utils.colorTranslate(Texts.tablist_header), Utils.colorTranslate(Texts.tablist_footer));
 		
 		// ** Return
 		return;
@@ -38,5 +47,33 @@ public class PlayerJoin
 		
 		// ** Return
 		return;
+	}
+
+	// ** sendHeaderFooter
+	public static void sendHeaderFooter(Player player, String header, String footer)
+	{
+		// ** Tablist Header
+		IChatBaseComponent tabHeader = ChatSerializer.a("{\"text\": \"" + header + "\"}");
+		
+		// ** Tablist Footer
+		IChatBaseComponent tabFooter = ChatSerializer.a("{\"text\": \"" + footer + "\"}");
+		
+		PacketPlayOutPlayerListHeaderFooter packet = new PacketPlayOutPlayerListHeaderFooter();
+		try {
+			// ** Header field
+			Field headerField = packet.getClass().getDeclaredField("a");
+			headerField.setAccessible(true);
+			headerField.set(packet, tabHeader);
+			headerField.setAccessible(false);
+			
+			// ** Footer field
+			Field footerField = packet.getClass().getDeclaredField("b");
+			footerField.setAccessible(true);
+			footerField.set(packet, tabFooter);
+			footerField.setAccessible(false);
+		} catch (Exception e) { e.printStackTrace(); }
+
+		// ** Send Packet
+		((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
 	}
 }
