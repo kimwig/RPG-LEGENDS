1b13ef735efcfafa93fe272bf252b6d0e478cf76
Happyfaceing <wallsylt@gmail.com>
2019-06-14 14:44:45 +0200

    Update Texts.java

--- RPG-LEGENDS/src/rpg/dafney/legends/functions/Texts.java
+++ RPG-LEGENDS/src/rpg/dafney/legends/functions/Texts.java
@@ -7,6 +7,9 @@ public class Texts
 	public static String server_url = "&9&nhttps://github.com/DafneyAtlas/RPG-LEGENDS";
 	public static String server_motd = server_name + "&r " + server_subname + "&r\n" + server_url + "&r";
 
+	public static String tablist_header = server_name + "&r " + server_subname + "&r\n";
+	public static String tablist_footer = "&r\n" + server_url + "&r";
+
 	public static String player_join_message = Tags.gray + "%player% logged in";
 	public static String player_join_message_op = Tags.pink + "%player% logged in";
 	public static String player_quit_message = Tags.gray + "%player% logged out";
